28ef44309253d06ed6b81986eca4ecb448543969
matiasGuidone <matias.guidone@gmail.com>
2020-08-11 19:09:56 -0300

    conexion de datos, ejemplo de creación de usuario con kivy

--- modelo/conexion.py
+++ modelo/conexion.py
@@ -6,72 +6,72 @@ class conexion:
     con=sqlite3.connect("data-ofelia.db")
 
     def insert_case(self, argument):
-        switcher = {
-            "Usuarios": "insert into Usuarios (nombre_usu,tipo_usu,contraseña) values (?,?,?)",
-            "": "insert into ",
-            "": "insert into ",
-            "": "insert into ",
-            "": "insert into ",
-            "": "insert into ",
-            "": "insert into ",
-            "": "insert into ",
-            "": "insert into ",
-        }
+            if argument == "Usuarios" : return "insert into Usuarios (nombre_usu,tipo_usu,contraseña) values (?,?,?)"
+            elif argument == "CategoriaGastos": return "insert into CategoriaGastos (nombre_cat_gasto) values (?)"
+            elif argument == "SubcategoriaGastos": return "insert into SubcategoriaGastos ( nomb_subcat, descr_subcat, empleado_id, proveedor_id, sueldo, adelanto, contacto, f_h_adelanto, f__h_pago, CategoriaGastos_id_cat_gasto, cuenta) values (?,?,?,?,?,?,?,?,?,?,?)"
+            elif argument == "Turnos": return "insert into Turnos (turno, caja_ini_turno, fecha, f_h_apertura, f_h_cierre, Usuarios_id_usu) values (?,?,?,?,?,?)"
+            elif argument == "TiposDePagos": return "insert into TiposDePagos (nombre_tipo_pago) values (?)"
+            elif argument == "Ventas": return "insert into Ventas (f_h_venta, monto_venta, borrado, f_h_borrado, observacion, Turnos_id_turno, TiposDePagos_id_tipo_pago, caja ) values (?,?,?,?,?,?,?,?)"
+            elif argument == "CategoriaProductos": return "insert into CategoriaProductos (descr_categ) values (?)"
+            elif argument == "ProductosPorMayor": return "insert into ProductosPorMayor (nombre_producto, monto_producto, CategoriaProductos_id_cat_prod ) values (?,?,?)"
+            elif argument == "Gastos": return "insert into Gastos (monto_gasto, observacion_gasto, TiposDePagos_id_tipo_pago, Turnos_id_turno, SubcategoriaGastos_id_subcat_gasto) values (?,?,?,?,?)"
+        
 
     def update_case(self, argument):
-        switcher = {
-            "Usuarios": "update Usuarios set nombre_usu = ? ,tipo_usu = ? ,contraseña = ? where id_usu = ?",
-            "": "update ",
-            "": "update ",
-            "": "update ",
-            "": "update ",
-            "": "update ",
-            "": "update ",
-            "": "update ",
-            "": "update "
-        }
+            if argument == "Usuarios": return "update Usuarios set nombre_usu = ? ,tipo_usu = ? ,contraseña = ? where id_usu = ?"
+            elif argument == "CategoriaGastos": return "update CategoriaGastos set nombre_cat_gasto = ? where id_cat_gasto = ?"
+            elif argument == "SubcategoriaGastos": return "update SubcategoriaGastos set nomb_subcat = ?, descr_subcat = ?, empleado_id = ?, proveedor_id = ?, sueldo = ?, adelanto = ?, contacto = ?, f_h_adelanto = ?, f__h_pago = ?, CategoriaGastos_id_cat_gasto = ?, cuenta = ? where id_subcat_gasto = ?"
+            elif argument == "Turnos": return "update Turnos set turno = ?, caja_ini_turno = ?, fecha = ?, f_h_apertura = ?, f_h_cierre = ?, Usuarios_id_usu = ? where id_turno = ?"
+            elif argument == "TiposDePagos": return "update TiposDePagos set nombre_tipo_pago = ? where id_tipo_pago = ?"
+            elif argument == "Ventas": return "update Ventas set f_h_venta = ?, monto_venta = ?, borrado = ?, f_h_borrado = ?, observacion = ?, Turnos_id_turno = ?, TiposDePagos_id_tipo_pago = ?, caja = ?  where id_venta = ?"
+            elif argument == "CategoriaProductos": return "update CategoriaProductos set descr_categ = ? where id_cat_prod = ?"
+            elif argument == "ProductosPorMayor": return "update ProductosPorMayor set nombre_producto = ?, monto_producto = ?, CategoriaProductos_id_cat_prod = ? where id_productos = ?"
+            elif argument == "Gastos": return "update Gastos set monto_gasto = ?, observacion_gasto = ?, TiposDePagos_id_tipo_pago = ?, Turnos_id_turno = ?, SubcategoriaGastos_id_subcat_gasto = ? where id_gasto = ?"
+       
 
     def selectId_case(self, argument):
-        switcher = {
-            "Usuarios": "select * from Usuarios where id_usu = ?",
-            "": "select * from ",
-            "": "select * from ",
-            "": "select * from ",
-            "": "select * from ",
-            "": "select * from ",
-            "": "select * from ",
-            "": "select * from ",
-            "": "select * from "
-        }
+            if argument == "Usuarios": return "select * from Usuarios where id_usu = ?"
+            elif argument == "CategoriaGastos": return "select * from CategoriaGastos where id_cat_gasto = ?"
+            elif argument == "SubcategoriaGastos": return "select * from SubcategoriaGastos where id_subcat_gasto = ?"
+            elif argument == "Turnos": return "select * from Turnos where id_turno = ?"
+            elif argument == "TiposDePagos": return "select * from TiposDePagos where id_tipo_pago = ?"
+            elif argument == "Ventas": return "select * from Ventas where id_venta = ?"
+            elif argument == "CategoriaProductos": return "select * from CategoriaProductos where id_cat_prod = ?"
+            elif argument == "ProductosPorMayor": return "select * from ProductosPorMayor where id_productos = ?"
+            elif argument == "Gastos": return "select * from Gastos where id_gasto = ?"
+         
 
     def delete_case(self, argument):
-        switcher = {
-            "Usuarios": "delete from Usuarios where id_usu = ?",
-            "": "delete from ",
-            "": "delete from ",
-            "": "delete from ",
-            "": "delete from ",
-            "": "delete from ",
-            "": "delete from ",
-            "": "delete from ",
-            "": "delete from "
-        }
+            if argument == "Usuarios": return "delete from Usuarios where id_usu = ?"
+            elif argument == "CategoriaGastos": return "delete from CategoriaGastos where id_cat_gasto = ?"
+            elif argument == "SubcategoriaGastos": return "delete from SubcategoriaGastos where id_subcat_gasto = ?"
+            elif argument == "Turnos": return "delete from Turnos where id_turno = ?"
+            elif argument == "TiposDePagos": return "delete from TiposDePagos where id_tipo_pago = ?"
+            elif argument == "Ventas": return "delete from Ventas where id_venta = ?"
+            elif argument == "CategoriaProductos": return "delete from CategoriaProductos where id_cat_prod = ?"
+            elif argument == "ProductosPorMayor": return "delete from ProductosPorMayor where id_productos = ?"
+            elif argument == "Gastos": return "delete from Gastos where id_gasto = ?"
  
 
     def insert(self, parameter_list, tabla):
-        con.execute(insert_case(tabla),parameter_list)
+        con=sqlite3.connect("data-ofelia.db")
+        con.execute(self.insert_case(tabla)[0],parameter_list)
     
     def update(self, parameter_list, tabla):
-        con.execute(insert_case(tabla),parameter_list)
+        con=sqlite3.connect("data-ofelia.db")
+        con.execute(self.update_case(tabla)[0],parameter_list)
 
     def delete(self, id, tabla):
-        con.execute(delete_case(tabla),id)
+        con=sqlite3.connect("data-ofelia.db")
+        con.execute(delete_case(tabla)[0],id)
 
     def selectId(self, id, tabla):
-        cursor=con.execute(selectId_case(tabla),id)
+        con=sqlite3.connect("data-ofelia.db")
+        cursor=con.execute(selectId_case(tabla)[0],id)
         return cursor
 
     def selectAll(self, tabla):
+        con=sqlite3.connect("data-ofelia.db")
         cursor=con.execute("select * from "+tabla)
         return cursor
             
--- vista/usuario.py
+++ vista/usuario.py
@@ -1,17 +1,31 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+import sys, os
+sys.path.append(os.getcwd())
+from modelo.conexion import conexion 
 # configuration
 from kivy.config import Config
-Config.set("graphics", "width",  800)
-Config.set("graphics", "height", 600)
+Config.set("graphics", "width",  540)
+Config.set("graphics", "height", 200)
 
 class Box(BoxLayout):
-	pass
+    pass
+    def btnCancelar(self):
+        print ('Aceptar')
+
+    def btnAceptar(self): 
+        usuario_text = self.ids.txt_usuario.text
+        pass_text = self.ids.txt_pass.text
+        cone = conexion()
+        param = [usuario_text, "admin", pass_text]
+        cone.insert(param,"Usuarios")
+        
+	
 
 class UsuarioApp(App):
-	def build(self):
-		  		return Box()  
+    def build(self):
+		  		return Box() 
 
 if __name__ == "__main__":
 	UsuarioApp().run()
